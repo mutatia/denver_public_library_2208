ad0a0bbd196aacc64ac7045e790b70d69b458834
Patricia Severance <patriciaseverance@gmail.com>
2022-09-14 09:08:31 -0600

    Add book_spec and book files

---
+++ spec/book_spec.rb
@@ -0,0 +1,11 @@
+require './lib/book'
+
+describe Book do
+  it 'exists and has readable attributes' do
+    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+    expect(book).to be_an_instance_of Book
+    expect(book.title).to eq "To Kill A Mockingbird"
+    expect(book.author).to eq "Harper Lee"
+    expect(book.publication_year).to eq "1960"
+  end
+end
